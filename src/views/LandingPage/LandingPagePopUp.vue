de210a95c04d9781d327880f3a33b7836c2ce9be
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-09-05 04:33:53 +0700

    feat: added landing page

---
+++ src/views/LandingPage/LandingPagePopUp.vue
@@ -0,0 +1,26 @@
+<style>
+.landing-page-pop-up {
+    width: 665px;
+    height: 559px;
+
+    background: white;
+    border-radius: 16px;
+    padding-bottom: 70px;
+
+    display: grid;
+    grid-template-rows: 145px 520px;
+    grid-template-areas: 
+        "header"
+        "main";
+}
+</style>
+
+<template lang="pug">
+    div.landing-page-pop-up: slot
+</template>
+
+<script>
+export default {
+    name: 'LandingPagePopUp'
+}
+</script>
---
+++ src/views/LandingPage/index.vue
@@ -0,0 +1,55 @@
+<style scoped lang="scss">
+.main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    grid-area: main;
+
+    * {
+        margin-bottom: 20px;
+    }
+}
+.title{
+    width: 462px;
+    height: 76px;
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 35px;
+    line-height: 41px;
+    color: #363636;
+
+    margin-bottom: 50px;
+}
+.v-btn {
+    width: 452px;
+    height: 53px !important;
+    
+    font-family: Raleway;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px !important;
+    line-height: 30px;
+    text-transform: none;
+}
+</style>
+
+<template lang="pug">
+    LandingPagePopUp
+        div.main
+            div.title The Privacy-First Platform for Personal Genetic Testing
+            v-btn(elevation='0', color='#C400A5', dark) Sign In
+            v-btn(elevation='0', outlined, color='#C400A5') Generate Account
+</template>
+
+<script>
+import LandingPagePopUp from './LandingPagePopUp.vue'
+
+export default {
+    name: 'MainPage',
+    components: {
+        LandingPagePopUp,
+    },
+}
+</script>
