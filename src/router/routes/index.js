1aec7503fc03bf40814a9ff03b32e50ae6392f15
ciniiia <cinia@blocksphere.com>
2021-11-30 17:48:20 +0800

    fix: link to etherscan

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -71,7 +71,7 @@ const indexRoutes = [{
       component: () => import(/* webpackChunkName */ "@/views/Dashboard/Customer/Home/RequestTest/Checkout")
     },
     {
-      path: "request-test/success",
+      path: "request-test/success/:hash?",
       name: "customer-request-test-success",
       meta: { pageHeader: "Success", parent: "customer-request-test" },
       component: () => import(/* webpackChunkName */ "@/views/Dashboard/Customer/Home/RequestTest/Success")
