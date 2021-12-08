ad5e86f2aaea52601810b67b6773dfb7a9737c59
Dicki <bukandicki@gmail.com>
2021-12-08 13:47:47 +0700

    Minor update code

--- src/common/lib/utils/debounce/substrate/index.js
+++ src/common/lib/utils/debounce/substrate/index.js
@@ -1,6 +1,7 @@
 // TODO: Need additional configuration if needed
+let timeout
+
 export const substrateDebounce = (fn, delayTime, root = undefined) => {
-  let timeout
   return function (...args) {
     clearTimeout(timeout)
     timeout = setTimeout(() => fn.apply(root ? root : this, args), delayTime)
