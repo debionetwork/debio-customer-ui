f876c11e4d4a5bbced7d98520fff2c986e7d392b
Dicki <bukandicki@gmail.com>
2022-01-10 17:07:28 +0700

    Unit testing and fix dropdown

--- src/common/components/Modal/index.vue
+++ src/common/components/Modal/index.vue
@@ -49,7 +49,7 @@ export default {
     show: { type: Boolean, default: false },
     disabledIconAnimate: { type: Boolean, default: false },
     title: { type: String, default: "Default title" },
-    type: { type: String, default: "alert", validation: val => allowedType.test(val) },
+    type: { type: String, default: "alert", validator: val => allowedType.test(val) },
     icon: { type: String, default: null },
     iconSize: { type: [String, Number], default: 80 },
     iconViewBox: { type: String, default: "0 0 40 40" },
