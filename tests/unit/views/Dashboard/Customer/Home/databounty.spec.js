b188d73e45a39ff76f9345f8b33cbe449aa257aa
Dicki <bukandicki@gmail.com>
2022-01-26 14:21:07 +0700

    tests: Add basic unit tesing

---
+++ tests/unit/views/Dashboard/Customer/Home/databounty.spec.js
@@ -0,0 +1,8 @@
+import { mount } from "@vue/test-utils"
+import DataBounty from "@/views/Dashboard/Customer/Home/DataBounty"
+
+describe("Databounty page", () => {
+  it("Should render", () => {
+    mount(DataBounty)
+  })
+})
