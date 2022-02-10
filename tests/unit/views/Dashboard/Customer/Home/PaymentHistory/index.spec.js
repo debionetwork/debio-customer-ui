dc72f00b2e23dbd6f38ec91c1a7965a275a2c7b4
Azmi Muhammad <azmim85@gmail.com>
2022-02-10 07:55:07 +0700

    tests: add config

--- tests/unit/views/Dashboard/Customer/Home/PaymentHistory/index.spec.js
+++ tests/unit/views/Dashboard/Customer/Home/PaymentHistory/index.spec.js
@@ -3,11 +3,12 @@ import PaymentHistory from "@/views/Dashboard/Customer/Home/PaymentHistory"
 import Vuex from "vuex"
 import Vuetify from "vuetify"
 
+config.stubs["ui-debio-input"] = { template: "<div></div>" }
 config.stubs["ui-debio-icon"] = { template: "<div></div>" }
 config.stubs["ui-debio-modal"] = { template: "<div></div>" }
 config.stubs["ui-debio-banner"] = { template: "<div></div>" }
 
-describe("Customer EMR Dashboard", () => {
+describe("Customer Payment History Dashboard", () => {
   let container
   let localVue = null
 
