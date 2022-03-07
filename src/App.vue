a0f10ce801ee62cf1c27504411e7c0407fd218a1
Dicki Maulana <bukandicki@gmail.com>
2022-03-07 13:34:39 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -107,6 +107,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap');
 @import "@/common/styles/variables.scss";
 @import "@/common/styles/mixins.sass";
 
