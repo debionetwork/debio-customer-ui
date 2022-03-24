1d78b259f13c6478c731cb22b88589ab93cd6e09
Dicki <bukandicki@gmail.com>
2022-03-24 11:06:33 +0700

    Add title to speciment number

--- src/views/Dashboard/Customer/Home/PaymentHistory/index.vue
+++ src/views/Dashboard/Customer/Home/PaymentHistory/index.vue
@@ -21,7 +21,9 @@
             ui-debio-avatar(:src="item.service_info.image" size="41" rounded)
             .payment-history__item-details
               .payment-history__item-name {{ item.service_info.name }}
-              .payment-history__item-speciment {{ item.dna_sample_tracking_id || item.genetic_analysis_tracking_id }}
+              .payment-history__item-speciment(
+                :title="`Specimen Number / Tracking ID : ${item.dna_sample_tracking_id || item.genetic_analysis_tracking_id}`"
+              ) {{ item.dna_sample_tracking_id || item.genetic_analysis_tracking_id }}
 
         template(v-slot:[`item.service_info.prices_by_currency[0].total_price`]="{ item }")
           .payment-history__price-details
