cc681ea7c3877466e6faab92252ce60cfd17c55a
ciniiia <cinia@blocksphere.id>
2022-03-25 23:39:42 +0700

    feat: create page order details for genetic anlysis request

---
+++ src/views/Dashboard/Customer/Home/GeneticData/Details/index.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+  .ga-order-detail
+    .ga-order-detail__title Test Summary
+
+    .ga-order-detail__cards
+      ServiceCard
+      StepperStatusCard
+
+
+
+  
+</template>
+
+
+<script>
+import ServiceCard from "./ServiceCard"
+import StepperStatusCard from "./StepperStatusCard"
+
+export default {
+  name: "GAOrderDetail",
+
+  components: {
+    ServiceCard,
+    StepperStatusCard
+  }
+
+}
+</script>
+
+<style lang="sass" scoped>
+  @import "@/common/styles/mixins.sass"
+  @import "@/common/styles/functions.sass"
+
+
+  .ga-order-detail
+    &__title
+      display: flex
+      justify-content: center
+      align-items: center
+      letter-spacing: 0.0044em
+      margin-top: toRem(52px)
+      @include h6-opensans
+
+    &__cards
+      display: flex
+      padding: toRem(52px)
+      gap: toRem(14px)
+
+      
+
+</style>
