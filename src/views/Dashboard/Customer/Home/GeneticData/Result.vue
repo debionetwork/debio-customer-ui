beaac11d5b7049a96bc0cb4275c8234670ee9270
Dicki <bukandicki@gmail.com>
2022-04-21 16:11:42 +0700

    feat: Init result page

---
+++ src/views/Dashboard/Customer/Home/GeneticData/Result.vue
@@ -0,0 +1,216 @@
+<template lang="pug">
+  .genetic-data-result
+    .genetic-data-result__wrapper
+      fieldset.file-details
+        legend.file-details__title Analysis Info
+        h3.file-details__service.mb-4 {{ details.service.name }}
+        p.file-details__author.mb-6 {{ computeAnalystName }}
+        p.file-details__desc.mb-10 {{ details.geneticData.comment || "No comment yet" }}
+        p.file-details__date Last Update: {{ details.geneticData.lastUpdate }}
+
+      .document-viewer
+        .document-viewer__wrapper(
+          :class="{ 'document-viewer__wrapper--animated': isLoading }"
+        )
+          h3.customer-emr-details__viewer-loading.text-center(v-if="isLoading") {{ message }}
+          embed.customer-emr-details__viewer-content(
+              v-if="!isLoading && result"
+              :src="`${result}#view=fitH`"
+              type="application/pdf"
+            )
+</template>
+
+<script>
+import { mapState } from "vuex"
+import Kilt from "@kiltprotocol/sdk-js"
+import CryptoJS from "crypto-js"
+import { u8aToHex } from "@polkadot/util"
+import {
+  queryGeneticAnalystByAccountId,
+  queryGeneticAnalysisByGeneticAnalysisTrackingId,
+  queryGeneticAnalysisOrderById,
+  queryGeneticAnalystServicesByHashId
+} from "@debionetwork/polkadot-provider"
+import { downloadFile, decryptFile } from "@/common/lib/pinata-proxy"
+
+
+
+export default {
+  name: "GeneticDataResult",
+
+  data: () => ({
+    isLoading: false,
+    publicKey: null,
+    secretKey: null,
+    result: null,
+    message: "Please wait",
+    details: {}
+  }),
+
+  computed: {
+    ...mapState({
+      api: (state) => state.substrate.api,
+      mnemonicData: (state) => state.substrate.mnemonicData
+    }),
+
+    computeAnalystName() {
+      return `${this.details?.analyst?.firstName} ${this.details?.analyst?.lastName}`
+    }
+  },
+
+  watch: {
+    mnemonicData(val) {
+      if (val) this.initialData()
+    }
+  },
+
+  async created() {
+    if (this.mnemonicData) this.initialData()
+  },
+
+  methods: {
+    async initialData() {
+      const cred = Kilt.Identity.buildFromMnemonic(this.mnemonicData.toString(CryptoJS.enc.Utf8))
+
+      this.publicKey = u8aToHex(cred.boxKeyPair.publicKey)
+      this.secretKey = u8aToHex(cred.boxKeyPair.secretKey)
+
+      if (cred) await this.prepareData()
+    },
+
+    async prepareData() {
+      const { serviceId, sellerId, geneticAnalysisdTrackingId } = await queryGeneticAnalysisOrderById(this.api, this.$route.params.id)
+      const service = await queryGeneticAnalystServicesByHashId(this.api, serviceId)
+      const analyst = await queryGeneticAnalystByAccountId(this.api, sellerId)
+      const geneticData = await queryGeneticAnalysisByGeneticAnalysisTrackingId(this.api, geneticAnalysisdTrackingId)
+
+      const parseDate = (date) => {
+        return new Date(parseInt(date.replaceAll(",", ""))).toLocaleDateString("en-GB", {
+          day: "numeric",
+          month: "short",
+          year: "numeric"
+        })
+      }
+
+      this.details = {
+        service: service.info,
+        analyst: analyst.info,
+        geneticData: { ...geneticData, lastUpdate: parseDate(geneticData.updatedAt) }
+      }
+
+      this.parseResult(geneticData.reportLink)
+    },
+
+    async parseResult(recordLink) {
+      try {
+        this.isLoading = true
+
+        const pair = { publicKey: this.publicKey, secretKey: this.secretKey }
+        const { type, data } = await downloadFile(recordLink, true)
+
+        const decryptedFile = decryptFile(data, pair, type)
+        const fileBlob = window.URL.createObjectURL(new Blob([decryptedFile], { type }))
+
+        this.result = fileBlob
+      } catch {
+        this.message = "Oh no! Something went wrong. Please try again later"
+      } finally {
+        this.isLoading = false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  @import "@/common/styles/mixins.sass"
+  .genetic-data-result
+    padding: 15px
+    background: #ffffff
+    border-radius: 4px
+
+    &__wrapper
+      display: flex
+
+  .document-viewer
+    width: 100%
+
+    &__wrapper
+      display: flex
+      align-items: center
+      justify-content: center
+      padding: 22px
+      min-height: 500px
+      background: #F5F7F9
+      border-radius: 4px
+
+      &--animated
+        position: relative
+        overflow: hidden
+
+        &::before
+          content: ""
+          display: block
+          position: absolute
+          top: 0
+          left: 0
+          width: 300px
+          height: 100%
+          background: rgba(255, 255, 255, .5)
+          animation: shine infinite 1s
+
+          @keyframes shine
+            0%
+              transform: skew(25deg) translateX(-1000px)
+            100%
+              transform: skew(25deg) translateX(1000px)
+
+    &__loading
+      &::after
+        content: ""
+        animation: dots infinite 2s linear
+
+        @keyframes dots
+          0%
+            content: "."
+          50%
+            content: ".."
+          100%
+            content: "..."
+
+    &__content
+      width: 100%
+      min-height: 700px
+      border-radius: 4px
+
+  .modal-password
+    &__cta
+      gap: 20px
+
+  .file-details
+    height: max-content
+    margin-right: 35px
+    min-width: 255px
+    max-width: 260px
+    padding: 25px
+    border: solid 1px #6F4CEC
+    border-radius: 4px
+
+    &__title
+      padding: 0 5px
+      @include body-text-3
+
+    &__service
+      @include body-text-medium-1
+
+    &__author
+      @include body-text-3
+
+    &__desc
+      @include body-text-3
+
+    &__date
+      @include body-text-medium-3
+      margin-bottom: 0 !important
+
+</style>
