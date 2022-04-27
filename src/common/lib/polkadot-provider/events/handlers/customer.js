3db380d05afac2487d149df8cb8531e24c2061d6
Azmi Muhammad <azmim85@gmail.com>
2022-04-27 14:52:17 +0700

    feat: GA request flow notification

--- src/common/lib/polkadot-provider/events/handlers/customer.js
+++ src/common/lib/polkadot-provider/events/handlers/customer.js
@@ -69,6 +69,30 @@ const handler = {
     }
 
     return { data, id, params, wording}
+  },
+  geneticAnalysisOrders: async (dataEvent, value, valueMessage, event) => {
+    const data = dataEvent[0]
+    const id = data[value]
+    const status = data["status"]
+    const params = { orderId: id }
+    const formatedHash = `${id?.substr(0, 4)}...${id?.substr(id?.length - 4)}`
+    let wording = `${valueMessage} <${formatedHash}>`
+
+    if (event.method === "GeneticAnalysisOrderRefunded") {
+      wording = `${wording} has been ${status.toLowerCase()}, kindly check your account balance.`
+    }
+
+    return { data, id, params, wording }
+  },
+  geneticAnalysis: async (dataEvent, value, valueMessage) => {
+    const data = dataEvent[0]
+    const id = data[value]
+    const status = data["status"]
+    const params = { orderId: id }
+    const formatedHash = `${id?.substr(0, 4)}...${id?.substr(id?.length - 4)}`
+    const wording = `${valueMessage} <${formatedHash}> has been ${status.toLowerCase()}. Click here to see your order details.`
+
+    return { data, id, params, wording }
   }
 }
 
