9ef4a82007727b2523a874ce18016c91aceec006
Dicki <bukandicki@gmail.com>
2022-07-14 13:53:24 +0700

    feat: Add app version

--- src/common/components/NavigationDrawer.vue
+++ src/common/components/NavigationDrawer.vue
@@ -51,6 +51,9 @@
   display: flex
   font-weight: 400
 
+.version
+  @include body-text-3-opensans
+
 .footerDrawer a
   font-size: 13px
   color: grey
@@ -176,11 +179,27 @@
                 viewBox="0 0 18 18"
               )
               span Customer Care
+          a.mt-3.d-block(
+            href="https://docs.debio.network/"
+            target="_blank"
+            style="text-decoration: none"
+          )
+            div
+              ui-debio-icon.btnDrawerIcon(
+                :icon="alertIcon"
+                size="16"
+                stroke
+                :color="'#757274'"
+              )
+              span Terms and Condition
+          div.version.mt-16.mb-10.d-flex
+            span.mr-12 DeBio Network
+            span {{ version }}
 </template>
 
 <script>
 
-import { bookIcon } from "@debionetwork/ui-icons"
+import { bookIcon, alertIcon } from "@debionetwork/ui-icons"
 
 export default {
   name: "NavigationDrawer",
@@ -192,8 +211,15 @@ export default {
   },
 
   data: () => ({
-    bookIcon
+    bookIcon,
+    alertIcon,
+
+    version: ""
   }),
+
+  created() {
+    this.version = `v${process.env.VUE_APP_VERSION}`
+  },
 
   methods: {
     openHref(href){
@@ -201,9 +227,8 @@ export default {
     },
 
     customClass(active) {
-      if (active) {
-        return "font-weight-bold sidebar-text primary--text activeButton"
-      }
+      if (active) return "font-weight-bold sidebar-text primary--text activeButton"
+
       return "font-weight-bold sidebar-text"
     }
   }
