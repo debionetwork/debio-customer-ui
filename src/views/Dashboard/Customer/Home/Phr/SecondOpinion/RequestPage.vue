03dc6f3d6a7afdef896d8a82e1e6dda28770dffc
ciniiia <cinia@blockshpere.id>
2022-12-14 00:40:22 +0700

    feat: add pop up install polkadot extension

---
+++ src/views/Dashboard/Customer/Home/Phr/SecondOpinion/RequestPage.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .request-second-opinion
+    v-card.request-second-opinion__card
+      .request-second-opinion__card-title First Step, Describe your symptoms
+
+      v-divider.mt-5 
+</template>
+
+<script>
+export default {
+  name: "RequestPage"
+
+}
+</script>
+
+<style lang="sass" scoped>
+  @import "@/common/styles/mixins.sass"
+
+  .request-second-opinion
+    padding: 50px
+    width: 859px
+
+    &__card
+      padding: 24px 
+      padding-bottom: 500px
+
+    &__card-title
+      @include button-1
+
+
+
+
+</style>
